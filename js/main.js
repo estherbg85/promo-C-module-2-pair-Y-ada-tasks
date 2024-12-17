9600fb8468a0d77db9a27f87e7dd7442741f9780
estherbg85 <estherbonillagirol85@gmail.com>
2024-12-17 12:29:06 +0100

    finish

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
 const taskList = document.querySelector('.js_list');
 const btntNewTask = document.querySelector('.js_new_task_btn');
 const searchInput = document.querySelector('.js_search_input');
+const taskSubmit = document.querySelector('.js_task_submit');
 
 // SECCION DE LOS DATOS DE LA APLICACION
 
@@ -13,8 +14,10 @@ const taskArray = [
     { name: "Sacar al perro", completed: true, id: 1 },
     { name: "Echar gasolina", completed: true, id: 2 },
     { name: "Ir al supermercado", completed: true, id: 3 },
-    { name: "Pedir cita a la peluqueria para el lunes 25 ",completed: false, id: 4,},
+    { name: "Pedir cita a la peluqueria para el lunes 25 ",completed: false, id: 4,}
 ];
+
+let filteredTaskArray = [];
 
 // SECCION DE FUNCIONES
 
@@ -24,11 +27,13 @@ const handleClickTask = (ev) => {
 
     ev.currentTarget.classList.toggle('completed');
     console.log(ev.currentTarget);
+
+   // fillTasks();
 }
 
-const fillTasks = () => {
+const fillTasks = (taskFilter) => {
     taskList.innerHTML=``;
-    for( const task of taskArray) {
+    for( const task of taskFilter) {
     
         if (task.completed) {
             taskList.innerHTML += `<li class="js_input list_tasks completed"><input class="checkbox" 
@@ -56,11 +61,16 @@ input.addEventListener('click', handleClickTask);
 
 
 const handleSearchInput = (ev) => {
-    console.log('estoy escribiendo');
-        
-    }
 
-fillTasks();
+    console.log(searchInput.value);
+    //filteredTaskArray.concat(taskArray.filter((taskObj) => taskObj.name.includes(searchInput.value)));
+    filteredTaskArray = taskArray.filter((taskObj) => taskObj.name.includes(searchInput.value)).slice();
+    console.log(filteredTaskArray);
+       
+}   
+ 
+
+fillTasks(taskArray);
 
 
 
@@ -71,19 +81,23 @@ btntNewTask.addEventListener('click', (ev) => {
     
     const newTask = [];
 
-    const newTaskInput = document.querySelector('.js_new_task_input').value;
-    newTask.push(newTaskInput);
+    const newTaskInput = document.querySelector('.js_new_task_input');
+    newTask.push(newTaskInput.value);
 
     const addTask = 
-    { name: newTaskInput, completed: false, id: taskArray.length+1 };
+    { name: newTaskInput.value, completed: false, id: taskArray.length+1 };
 
     taskArray.push(addTask);
     
-    fillTasks();
+    fillTasks(taskArray);
 
+    newTaskInput.value ='';
 });
 
-
+taskSubmit.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    fillTasks(filteredTaskArray); 
+ });
 
 
 searchInput.addEventListener('input', handleSearchInput);
